7b16c92a78a73bf3c56c96e6df7b053439aa78a0
Lu <lucien.brunet@gmail.com>
2016-07-04 00:25:45 +0100

    cache gametable layers on server

--- app/Broadcast.php
+++ app/Broadcast.php
@@ -6,6 +6,9 @@ use Ratchet\ConnectionInterface;
 
 class Broadcast implements MessageComponentInterface {
 	protected $clients;
+    protected $background_items;
+    protected $foreground_items;
+    protected $actors;
 
 	public function __construct() {
 		$this->clients = new \SplObjectStorage;
@@ -14,6 +17,9 @@ class Broadcast implements MessageComponentInterface {
 	public function onOpen(ConnectionInterface $conn) {
 		$this->clients->attach($conn);
 		echo "new connection! ({$conn->resourceId}) \n";
+        $conn->send($background_items);
+        $conn->send($foreground_items);
+        $conn->send($actors);
 	}
 
 	public function onMessage(ConnectionInterface $from, $msg) {
@@ -22,17 +28,28 @@ class Broadcast implements MessageComponentInterface {
 				$client->send($msg);
 			}
 		}
+        //if it's a canvas layer, cache it.
+        $passed_obj = json_decode($msg);
+        if ($passed_obj[0].obj_type == 'box'){
+            $background_items = $msg;
+        }
+        if ($passed_obj[0].obj_type == 'line'){
+            $foreground_items = $msg;
+        }
+        if ($passed_obj[0].obj_type == 'actor'){
+            $actors = $msg;
+        }
 	}
-	
+
 	public function onClose(ConnectionInterface $conn){
 		$this->clients->detach($conn);
-		
+
 		echo "Connection closed \n";
 	}
 
 	public function onError(ConnectionInterface $conn, \Exception $e) {
 		echo "an error has occured: {$e->getMessage()} \n";
-		
+
 		$conn->close();
 	}
 }
