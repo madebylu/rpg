f012919dce9701d2e4e014dc2517c0625a79098f
Lu <lucien.brunet@gmail.com>
2016-11-14 13:07:35 +0000

    added links to setting information aside

--- resources/views/setting.blade.php
+++ resources/views/setting.blade.php
@@ -10,15 +10,18 @@
         <ul>
             <li><strong>Setting Information</strong> <a href="/setting/add">+</a></li>
             @foreach($setting_information as $section)
-            <li>{{$section->section}}.{{$section->subsection}} - {{$section->title}}</li>
+            <li>
+                <a href="#Setting{{$section->section}}.{{$section->subsection}}">
+                {{$section->section}}.{{$section->subsection}} - {{$section->title}}</a>
+            </li>
             @endforeach
             <li><strong>Places</strong> <a href="/place/add">+</a></li>
             @foreach($places as $place)
-            <li>{{$place->title}}</li>
+            <li><a href="#Place{{$place->title}}">{{$place->title}}</a></li>
             @endforeach
             <li><strong>Faiths</strong> <a href="/faith/add">+</a></li>
             @foreach($faiths as $faith)
-            <li>{{$faith->title}}</li>
+            <li><a href="#Faith{{$faith->title}}">{{$faith->title}}</a></li>
             @endforeach
 
         </ul>
@@ -28,15 +31,18 @@
         <h2>Setting stuff goes here</h2>
 
         @foreach($setting_information as $section)
-            <h2>{{$section->section}}.{{$section->subsection}} - {{$section->title }}
-                <span class="edit"><a href="/setting/edit/{{$section->id}}">~</a></span></h2>
+            <h2 id="Setting{{$section->section}}.{{$section->subsection}}">
+                {{$section->section}}.{{$section->subsection}} - {{$section->title }}
+                <span class="edit"><a href="/setting/edit/{{$section->id}}">~</a></span>
+            </h2>
             <p>{!! nl2br($section->content) !!}</p>
         @endforeach
 
         <h2>Places</h2>
         @foreach($places as $place)
-            <h2>{{$place->title}}
-                <span class="edit"><a href="/place/edit/{{$place->id }}">~</a></span></h2>
+            <h2 id="Place{{$place->title}}">{{$place->title}}
+                <span class="edit"><a href="/place/edit/{{$place->id }}">~</a></span>
+            </h2>
             <p>Key Words: {{$place->key_words}}</p>
             <p>Terrain: {{$place->terrain}}</p>
             <p>Rulership: {{$place->rulership}}</p>
@@ -46,7 +52,7 @@
 
         <h2>Faiths</h2>
         @foreach($faiths as $faith)
-            <h2>{{$faith->title}} - {{$faith->honourific}}
+            <h2 id="Faith{{$faith->title}}">{{$faith->title}} - {{$faith->honourific}}
                 <span class="edit"><a href="/faith/edit/{{$faith->id}}">~</a><span></h2>
             <p>Beliefs: {{$faith->beliefs}}</p>
             <p>Structure: {{$faith->structure}}</p>
